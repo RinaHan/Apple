6dd00c23cf0bb031e98586962051feddc03319b4
RinaHan <rinahan4146@gmail.com>
2024-02-19 19:13:36 -0800

    add animation effect to search bar

--- js/main.js
+++ js/main.js
@@ -30,10 +30,12 @@ function hideBasket() {
 
 // Search
 const headerEl = document.querySelector("header");
+const headerMunuEls = [...headerEl.querySelectorAll("ul.menu > li")];
 const searchWrapEl = headerEl.querySelector(".search-wrap");
 const searchStarterEl = headerEl.querySelector(".search-starter");
 const searchCloserEl = searchWrapEl.querySelector(".search-closer");
 const searchShadowEl = searchWrapEl.querySelector(".shadow");
+const searchDelayEls = [...searchWrapEl.querySelectorAll("li")];
 
 searchStarterEl.addEventListener("click", showSearch);
 searchCloserEl.addEventListener("click", hideSearch);
@@ -42,8 +44,21 @@ searchShadowEl.addEventListener("click", hideSearch);
 function showSearch() {
   headerEl.classList.add("searching");
   document.documentElement.classList.add("fixed");
+  headerMunuEls.reverse().forEach(function (el, index) {
+    el.style.transitionDelay = (index * 0.5) / headerMunuEls.length + "s";
+  });
+  searchDelayEls.forEach(function (el, index) {
+    el.style.transitionDelay = (index * 0.5) / searchDelayEls.length + "s";
+  });
 }
 function hideSearch() {
   headerEl.classList.remove("searching");
   document.documentElement.classList.remove("fixed");
+  headerMunuEls.reverse().forEach(function (el, index) {
+    el.style.transitionDelay = (index * 0.5) / headerMunuEls.length + "s";
+  });
+  searchDelayEls.reverse().forEach(function (el, index) {
+    el.style.transitionDelay = (index * 0.5) / searchDelayEls.length + "s";
+  });
+  searchDelayEls.reverse();
 }
